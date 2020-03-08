f3d954eb23dd472c143d5f164dbd95ab9acee7dd
dazzlight <adelina08042004@gmail.com>
2020-03-08 22:30:45 +0200

    Аdded function to change color using text field

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,14 @@
 function turnOn() {
-  document.body.style.backgroundColor = "white";
   const bulbOnText = document.getElementById("lightText");
+  let color = document.getElementsByName("color")[0].value;
+  if (!color) {
+    color = "#ffbe00";
+  }
   const bulbOn = document.querySelector(".fas, .fa-lightbulb");
+  bulbOn.style.color = color;
+  document.body.style.backgroundColor = "white";
   bulbOnText.innerHTML = "Bulb is: ON";
   bulbOnText.style.color = "black";
-  bulbOn.style.color = "yellow";
 }
 
 function turnOff() {
