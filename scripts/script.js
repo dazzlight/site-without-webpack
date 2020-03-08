03ac1804d57789ad1a39ab793204aa85ec94fd29
dazzlight <adelina08042004@gmail.com>
2020-03-08 21:43:53 +0200

    Had made project "Light turn ON/ turn OFF"

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,17 @@
-alert("It is my page");
+function turnOn() {
+  document.body.style.backgroundColor = "white";
+  const bulbOnText = document.getElementById("lightText");
+  const bulbOn = document.querySelector(".fas, .fa-lightbulb");
+  bulbOnText.innerHTML = "Bulb is: ON";
+  bulbOnText.style.color = "black";
+  bulbOn.style.color = "yellow";
+}
+
+function turnOff() {
+  document.body.style.backgroundColor = "grey";
+  const bulbOnText = document.getElementById("lightText");
+  const bulbOn = document.querySelector(".fas, .fa-lightbulb");
+  bulbOnText.innerHTML = "Bulb is: OFF";
+  bulbOnText.style.color = "white";
+  bulbOn.style.color = "black";
+}
