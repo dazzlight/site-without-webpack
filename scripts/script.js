29428d7382bdbcbfd8df55d066d8b157cb6e710c
dazzlight <adelina08042004@gmail.com>
2020-03-21 15:18:01 +0200

    Tested operators;

--- scripts/script.js
+++ scripts/script.js
@@ -1,185 +1,48 @@
-const painters = [
+const age = 17;
+
+// 1) Проверьте переменную age и если возраст больше или равен 18 и меньше или равен 21, выведите в консоль соответсвующее сообщение
+age >= 18 || age <= 21 ? console.log("true") : console.log("false");
+
+const users = [
   {
-    firstName: "Herb",
-    lastName: "Aach",
-    born: 1923,
-    passed: 1985,
-    paintings: 12
+    name: "Roman",
+    age: 28,
+    visitedPark: true,
+    boughtItems: false
   },
   {
-    firstName: "Pacita",
-    lastName: "Abad",
-    born: 1946,
-    passed: 2004,
-    paintings: 52
+    name: "Maya",
+    age: 14,
+    visitedPark: false,
+    boughtItems: true
   },
   {
-    firstName: "Daniel",
-    lastName: "Maclise",
-    born: 1806,
-    passed: 1870,
-    paintings: 2
+    name: "Alex",
+    age: 45,
+    visitedPark: false,
+    boughtItems: false
   },
   {
-    firstName: "Aileen",
-    lastName: "Eagleton",
-    born: 1902,
-    passed: 1984,
-    paintings: 35
-  },
-  {
-    firstName: "Thomas",
-    lastName: "Eakins",
-    born: 1844,
-    passed: 1916,
-    paintings: 1
-  },
-  {
-    firstName: "Edgar",
-    lastName: "Degas ",
-    born: 1834,
-    passed: 1917,
-    paintings: 34
-  },
-  {
-    firstName: "Martin",
-    lastName: "Desjardins",
-    born: 1637,
-    passed: 1694,
-    paintings: 7
+    name: "Tonia",
+    age: 32,
+    visitedPark: true,
+    boughtItems: true
   }
 ];
 
-const clients = [
-  "Fiona, Bownd",
-  "Bren, Strutt",
-  "Cletis, Cobelli",
-  "Booth, Kedge",
-  "Demetris, Parman",
-  "Cull, Itzhaki",
-  "Ertha, Bikker",
-  "Maggi, Stockport",
-  "Horace, Hearthfield",
-  "Vassili, Pomfret",
-  "Cirilo, Zottoli",
-  "Trueman, MacDermot",
-  "Karla, Spencer",
-  "Allys, McManamon",
-  "Saloma, Boolsen",
-  "Giacomo, Vedntyev",
-  "Essa, Blacket",
-  "Dari, Muncer",
-  "Jobi, Joscelyn",
-  "Eimile, Joberne"
-];
-
-const cars = [
-  "Honda",
-  "Saab",
-  "Ford",
-  "Mitsubishi",
-  "Buick",
-  "Toyota",
-  "Mitsubishi",
-  "Honda",
-  "Mercedes-Ben",
-  "Buick",
-  "Volvo",
-  "Honda",
-  "Oldsmobile",
-  "Mercedes-Ben",
-  "Chevrolet",
-  "Volkswagen",
-  "GMC",
-  "Chevrolet",
-  "Jeep",
-  "Acura",
-  "Acura",
-  "Suzuki"
-];
-
-console.table(painters);
-// 1. Array Length
-
-console.log("painters lenth", painters.length);
-console.log("clients lenth", clients.length);
-console.log("cars lenth", cars.length);
-
-// 2. Array Iteration with FOR & foreach
-
-// for (let i = 0; i <= painters.length; i++) {
-//   console.log(painters[i]);
-// }
-
-// painters.forEach(painter => {
-//   console.log(painter);
-// });
-
-// // 3. String to array .split
-
-// const myFavoriteFood = "Pizza, Pasta, Burger";
-// console.log(myFavoriteFood.split(", "));
-// const myFavoriteSport = ["Football", "Hokey", "Yoga"];
-// console.log(myFavoriteSport.join(", "));
-
-// 4. Array .filter()
-
-const filteredPainters = painters.filter(
-  painter => painter.born >= 1800 && painter.born < 1900
-);
-
-console.table(filteredPainters);
-// 5. Array .map()
-
-const paintersWithYears = painters.map(painter => {
-  return {
-    name: `${painter.firstName} ${painter.lastName}`,
-    years: `${painter.born} - ${painter.passed}`
-  };
-});
-console.table(paintersWithYears);
-// 6. Array .sort()
-// Sort painters by years lived
-const sorteredPainters = painters
-  .map(painter => {
-    return {
-      name: `${painter.firstName} ${painter.lastName}`,
-      years: `${painter.born} - ${painter.passed}`,
-      age: painter.passed - painter.born
-    };
-  })
-  .sort(
-    (currentPainter, nextPainter) =>
-      currentPainter.age > nextPainter.age ? -1 : 1
-    // if (currentPainterYears > nextPainterYears) {
-    //   return -1;
-    // } else {
-    //   return 1;
-    // }
-  );
-console.table(sorteredPainters);
-
-// Sort client by lastName
-const sortedClients = clients.sort((lastClient, nextClient) => {
-  const [lastClientFirstName, lastClientLastName] = lastClient.split(", ");
-  const [nextClientFirstName, nextClientLastName] = nextClient.split(", ");
-  return lastClientLastName > nextClientLastName ? 1 : -1;
-});
-console.table(sortedClients);
-
-// 7. Array .reduce()
-// Total amound of paintings
-const totalPaintings = painters.reduce((amount, painter) => {
-  return amount + painter.paintings;
-}, 0);
-console.log("Total paintings: ", totalPaintings);
-
-// Sum app cars
-const carsReduced = cars.reduce((obj, car) => {
-  if (!obj[car]) {
-    obj[car] = 0;
+// 2) Для каждого элемента массива users выведите сообщение в консоль, только если пользователь посетил парк или совершил покупку
+// Используйте foreach или for, а потом if внутри петли
+users.forEach(user => {
+  if (user.visitedPark == true || user.boughtItems == true) {
+    console.log(user);
   }
-  obj[car]++;
-  return obj;
-}, {});
-console.log(carsReduced);
+});
+
+let iterationCounter = "Each iteration adds a *: ";
+for (let i = 0; i < 10; i++) {
+  iterationCounter += " *";
+
+  // 3) Используя assigment operator(+=) поменяйте значение переменоой iterationCounter, добавляя по одной звездочке при каждой итерации
+}
+// 4) Выведите в консоль значение iterationCounter. Ожидаемый результат "Each iteration adds a *: * * * * * * * * * *"
+console.log(iterationCounter);
